613ef27feeeaf92f6d7d2fb14be39c5c2c9b8856
jmason87 <joewebdev87@gmail.com>
2023-03-02 19:29:37 -0600

    added player CRUD endpoints and controller

---
+++ app/Http/Controllers/PlayerController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Player;
+use App\Transformers\PlayerTransformer;
+use Illuminate\View\View;
+
+class PlayerController extends Controller
+{
+    public function index(Request $request)
+    {
+        return Player::all();
+    }
+    public function show(Request $request, $uuid)
+    {
+        return Player::findOrFail($uuid);
+    }
+    public function store(Request $request)
+    {
+        $player = new Player;
+        $player->name = $request->input('name');
+        $player->position = $request->input('position');
+        $player->age = $request->input('age');
+        $player->birthdate = $request->input('birthdate');
+        $player->year_drafted = $request->input('year_drafted');
+        $player->round = $request->input('round');
+        $player->pick = $request->input('pick');
+        $player->set_uuid = $request->input('set_uuid');
+        $player->save();
+    }
+    public function update(Request $request, $uuid)
+    {
+        $player = Player::findOrFail($uuid);
+        $player->name = $request->input('name');
+        $player->save();
+    }
+    public function destroy(Request $request, $uuid)
+    {
+        $player = Player::findOrFail($uuid);
+        $player->delete();
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\PlayerController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +16,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::controller(PlayerController::class)->group(function () {
+    Route::get('/players', 'index');
+    Route::get('/players/{uuid}', 'show');
+    Route::post('/players', 'store');
+    Route::put('/players/{uuid}', 'update');
+    Route::delete('/players/{uuid}', 'destroy');
 });
